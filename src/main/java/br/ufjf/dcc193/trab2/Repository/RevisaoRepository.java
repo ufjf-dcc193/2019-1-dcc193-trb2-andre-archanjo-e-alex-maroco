99e73181024227e8b7a759059b128913f1dd7558
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-15 13:37:22 -0300

    Revisao Repository

---
+++ src/main/java/br/ufjf/dcc193/trab2/Repository/RevisaoRepository.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trab2.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trab2.Model.Revisao;
+
+/**
+ * RevisaoRepository
+ */
+public interface RevisaoRepository extends JpaRepository<Long, Revisao>{
+
+    
+}
