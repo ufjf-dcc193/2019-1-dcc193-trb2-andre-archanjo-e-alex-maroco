1a1d4715ca1d8b43ecb961480e49d4315fb99475
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-15 13:33:30 -0300

    Classe Revisão

---
+++ src/main/java/br/ufjf/dcc193/trab2/Model/Revisao.java
@@ -0,0 +1,53 @@
+package br.ufjf.dcc193.trab2.Model;
+
+/**
+ * Revisão
+ */
+public class Revisao {
+
+    private Long id;
+
+    private int nota;
+
+    private String descricao;
+
+    private int status;
+
+    public Revisao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    
+
+}
