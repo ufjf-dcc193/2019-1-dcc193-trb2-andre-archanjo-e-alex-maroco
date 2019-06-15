5ab1cb43ebc948fed4ca51f88170b6c86ff70deb
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-15 13:37:11 -0300

    Avaliador Repository

---
+++ src/main/java/br/ufjf/dcc193/trab2/Repository/AvaliadorRepository.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trab2.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trab2.Model.Avaliador;
+
+/**
+ * AvaliadorRepository
+ */
+public interface AvaliadorRepository extends JpaRepository<Long, Avaliador>{
+
+    
+}
