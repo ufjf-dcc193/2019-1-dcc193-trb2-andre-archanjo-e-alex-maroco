8d27aeacec05ab24f75831f31dfc9008215c79e2
Alex Maroco <alexsmaroco@gmail.com>
2019-06-16 16:56:39 -0300

    Remove importações não utilizadas

--- src/main/java/br/ufjf/dcc193/trab2/controller/HomeController.java
+++ src/main/java/br/ufjf/dcc193/trab2/controller/HomeController.java
@@ -1,19 +1,9 @@
 package br.ufjf.dcc193.trab2.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.ufjf.dcc193.trab2.model.Avaliador;
-import br.ufjf.dcc193.trab2.repository.AvaliadorRepository;
 
 /**
  * HomeController
