72237faafa051327a0573605ecd6a20073b5c0c3
Alex Maroco <alexsmaroco@gmail.com>
2019-06-19 01:41:51 -0300

    Ordena trabalhos por quantidade de revisões 'avaliadas'

--- src/main/java/br/ufjf/dcc193/trab2/controller/TrabalhoController.java
+++ src/main/java/br/ufjf/dcc193/trab2/controller/TrabalhoController.java
@@ -1,6 +1,13 @@
 package br.ufjf.dcc193.trab2.controller;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 
 import javax.validation.Valid;
 
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.ufjf.dcc193.trab2.model.Revisao;
 import br.ufjf.dcc193.trab2.model.Trabalho;
 import br.ufjf.dcc193.trab2.repository.TrabalhoRepository;
 import br.ufjf.dcc193.trab2.service.LoginService;
@@ -107,14 +115,46 @@ public class TrabalhoController {
     @GetMapping(value={"/listar.html"}, params = {"area"})
     public ModelAndView listarArea(@RequestParam int area) {
         ModelAndView mv = new ModelAndView();
-        List<Trabalho> tr = tRepo.findAllByAreaConhecimento(area);
-        for (Trabalho t : tr) {
-            System.err.println(t.toString());
+        List<Trabalho> tbr = tRepo.findAllByAreaConhecimento(area);
+        HashMap<Trabalho, Integer> contador = new HashMap<>();
+        int qtd = 0;
+        for (Trabalho t : tbr) {
+            qtd = 0;
+            for(Revisao r: t.getListRevisaoTrabalho()) {
+                if(r.getStatus() == 1) {
+                    qtd++;
+                }
+            }
+            contador.put(t, qtd);
         }
-        mv.addObject("trabalhos", tr);
+        Map<Trabalho, Integer> sorted = sortByValue(contador);
+        mv.addObject("hashmap", sorted);
         mv.addObject("area", area);
         mv.setViewName("list-trabalhos-area");
         return mv;
+    }
+
+    private HashMap<Trabalho, Integer> sortByValue(HashMap<Trabalho, Integer> hm) 
+    { 
+        // Create a list from elements of HashMap 
+        List<Map.Entry<Trabalho, Integer> > list = 
+               new LinkedList<Map.Entry<Trabalho, Integer>>(hm.entrySet()); 
+  
+        // Sort the list 
+        Collections.sort(list, new Comparator<Map.Entry<Trabalho, Integer> >() { 
+            public int compare(Map.Entry<Trabalho, Integer> o1,  
+                               Map.Entry<Trabalho, Integer> o2) 
+            { 
+                return (o1.getValue()).compareTo(o2.getValue()); 
+            } 
+        }); 
+          
+        // put data from sorted list to hashmap  
+        HashMap<Trabalho, Integer> temp = new LinkedHashMap<Trabalho, Integer>(); 
+        for (Map.Entry<Trabalho, Integer> aa : list) { 
+            temp.put(aa.getKey(), aa.getValue()); 
+        } 
+        return temp; 
     }
     
 }
